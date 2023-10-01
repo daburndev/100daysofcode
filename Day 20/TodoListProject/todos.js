5fb729fead83ceea2cbc5a88eb546009921f272b
daburndev <daburndev@gmail.com>
2023-10-02 01:58:27 +0700

    day 20

--- Day 20/TodoListProject/todos.js
+++ Day 20/TodoListProject/todos.js
@@ -1,25 +1,25 @@
-let input = prompt('what would you like to do?');
-const todos = ['Collect Chicken Eggs', 'Clean Litter Box'];
-while (input !== 'quit' && input !== 'q') {
-    if (input === 'list') {
-        console.log('*****************')
-        for (let i = 0; i < todos.length; i++) {
-            console.log(`${i}: ${todos[i]}`);
+let input = prompt('What would you like to do?');
+const todos= ['Get up early', 'Make lunchbox'];
+while(input !== 'quit' && input !== 'q'){
+    if(input === 'list'){
+        console.log('************')
+        for(let i = 0; i< todos.length; i++){
+            console.log(`${i}: ${todos[i]}`)
         }
-        console.log('*****************')
-    } else if (input === 'new') {
-        const newTodo = prompt('Ok, what is the new todo?');
+        console.log('************')
+    } else if ( input === 'new'){
+        const newTodo = prompt('Ok,What is the new todo?');
         todos.push(newTodo);
-        console.log(`${newTodo} added to the list!`)
-    } else if (input === 'delete') {
-        const index = parseInt(prompt('Ok, enter an index to delete:'));
-        if (!Number.isNaN(index)) {
-            const deleted = todos.splice(index, 1);
-            console.log(`Ok, deleted ${deleted[0]}`);
+        console.log(`${newTodo} is added to the list!`)
+    } else if ( input === 'delete'){
+        const index = parseInt(prompt('Ok, Enter an index to delete!'));
+        if(!Number.isNaN(index)){
+        const deleted = todos.splice(index, 1);
+        console.log(`Ok, deleted ${deleted[0]}`);
         } else {
-            console.log('Unknown index')
+            console.log('Unknown index');
         }
     }
-    input = prompt('what would you like to do?')
+    input = prompt('What would you like to do?')
 }
-console.log('OK QUIT THE APP!')
+console.log('OK YOU QUIT THE APP')
