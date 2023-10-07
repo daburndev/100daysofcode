d359a0b933ce4987986f5a7539b60267b707aea9
daburndev <daburndev@gmail.com>
2023-10-07 21:45:16 +0700

    Day 25

---
+++ Day 25/DOM_Intro_CODE/Manipulating/app.js
@@ -0,0 +1,12 @@
+const allLinks = document.querySelectorAll('a');
+
+// for (let link of allLinks) {
+//     link.innerText = 'I AM A LINK!!!!'
+// }
+
+
+for (let link of allLinks) {
+    link.style.color = 'rgb(0, 108, 134)';
+    link.style.textDecorationColor = 'magenta';
+    link.style.textDecorationStyle = 'wavy'
+}
