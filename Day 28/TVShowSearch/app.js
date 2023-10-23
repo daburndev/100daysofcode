9ce3f4b96786002e06ec563ae70cb1e7dd549c06
daburndev <daburndev@gmail.com>
2023-10-23 20:16:58 +0700

    Day 28 end

--- Day 28/TVShowSearch/app.js
+++ Day 28/TVShowSearch/app.js
@@ -1,6 +1,21 @@
 const form = document.querySelector('#searchForm');
-form.addEventListener('submit', function(e){
+form.addEventListener('submit', async function(e){
     e.preventDefault();
-    console.log(form.elements.query.value)
+    const searchTerm = form.elements.query.value;
+    const config = { params: {q: searchTerm}};
+    const res = await axios.get(`http://api.tvmaze.com/search/shows`, config);
+    makeImages(res.data)
+    form.elements.query.value ='';
 }
 )
+
+const makeImages = (shows) => {
+    for (let result of shows ){
+        if(result.show.image){
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+       
+    }
+}
