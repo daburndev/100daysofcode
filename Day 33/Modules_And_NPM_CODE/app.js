09cf2fcf1b84b924a92c3d5d12102078a3319596
daburndev <daburndev@gmail.com>
2023-11-20 00:54:59 +0700

    day33

---
+++ Day 33/Modules_And_NPM_CODE/app.js
@@ -0,0 +1,7 @@
+const { PI, square } = require('./math');
+
+
+const cats = require('./shelter')
+// console.log(PI)
+
+console.log("REQUIRED AN ENTIRE DIRECTORY!", cats)
---
+++ Day 33/Modules_And_NPM_CODE/shelter/index.js
@@ -0,0 +1,6 @@
+const blue = require('./blue')
+const sadie = require('./sadie')
+const janet = require('./janet')
+
+const allCats = [blue, sadie, janet]
+module.exports = allCats;
