df338746b5ed244b8e921acb21fab9399c5173c2
daburndev <daburndev@gmail.com>
2023-11-20 01:20:31 +0700

    day33

--- Day 33/Modules_And_NPM_CODE/Jokester/index.js
+++ Day 33/Modules_And_NPM_CODE/Jokester/index.js
@@ -1,9 +1,7 @@
 const jokes = require("give-me-a-joke");
 const colors = require("colors");
-const cowsay = require("cowsay");
 
 jokes.getRandomDadJoke(function (joke) {
     console.log(joke.rainbow);
 });
 
-console.log(cowsay,'helloooo')
