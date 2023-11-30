58766ee8ea0502ac90e83da81e6e980300a3249c
daburndev <daburndev@gmail.com>
2023-12-01 01:04:39 +0700

    crud implementation

--- Day 36/index.js
+++ Day 36/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const path= require('path');
+const methodOverride = require('method-override');
 const {v4: uuid} = require('uuid');
 
 app.use(express.urlencoded({extended: true }))
 app.use(express.json())
+app.use(methodOverride('_method'))
 app.set('views',path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -49,6 +51,26 @@ app.get('/comments/:id',(req,res)=>{
     const { id } =req.params;
     const comment = comments.find(c => c.id === id)
     res.render('comments/show', {comment})
+})
+
+app.get('/comments/:id/edit', (req,res)=>{
+    const { id } =req.params;
+    const comment = comments.find(c => c.id === id)
+    res.render('comments/edit', {comment})
+})
+
+app.patch('/comments/:id',(req,res)=>{
+    const { id } =req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments');
+})
+
+app.delete('/comments/:id', (req,res)=>{
+    const { id } =req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
 })
 
 app.get('/tacos',(req,res)=>{
